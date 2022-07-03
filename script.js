eeefb12f34d9a0b614e976f4bdcd91c42c7a034c
asraf-mridha <asrafmridha@gmail.com>
2022-07-03 23:47:38 +0600

    grade count by condition

--- script.js
+++ script.js
@@ -8,12 +8,38 @@
 
    alert("Please Push a number");
      }
-   
-
         var total= parseInt(_practicalmarks)+parseInt(_theorymarks);
       
         document.getElementById("ttm").value=total;
 
+        if(total>=80 && total<=100){
+
+            document.getElementById("grade").value= "A+";
+        }
+        else if(total>=70 && total<=79){
+
+            document.getElementById("grade").value= "A";
+        }
+        else if(total>=60 && total<=69){
+
+            document.getElementById("grade").value= "A-";
+        }
+        else if(total>=50 && total<=59){
+
+            document.getElementById("grade").value= "B";
+        }
+        else if(total>=40 && total<=49){
+
+            document.getElementById("grade").value= "C";
+        }
+        else if(total>=33 && total<=39){
+
+            document.getElementById("grade").value= "D";
+        }
+        else if(total>=0 && total<=32){
+
+            document.getElementById("grade").value= "F";
+        }
     
     
 
