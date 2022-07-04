43262de23d40ea5b077dc850f777aeac8fb37a4f
asraf-mridha <asrafmridha@gmail.com>
2022-07-04 08:07:39 +0600

    Create Function for English First paper

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ function total1(){
         }
         else if(total>=60 && total<=69){
 
-            document.getElementById("grade").value= "A-";
+            document.getElementById("grade1").value= "A-";
         }
         else if(total>=50 && total<=59){
 
@@ -168,9 +168,92 @@ function total1(){
             document.getElementById("point1").value= "0";
           
         }
-    
-    
-    
 
+ }
 
- }1
+//  Anothr Function for English 1st Paper 
+
+function total3(){
+
+    var _theorymarks= document.getElementById("tm3").value;
+    var _practicalmarks= document.getElementById("pm3").value;
+
+     if( _theorymarks=="" || _practicalmarks=="" ){
+
+   alert("Please Push a number");
+     }
+        var total= parseInt(_practicalmarks)+parseInt(_theorymarks);
+      
+        document.getElementById("ttm3").value=total;
+
+        if(total>=80 && total<=100){
+
+            document.getElementById("grade3").value= "A+";
+        }
+        else if(total>=70 && total<=79){
+
+            document.getElementById("grade3").value= "A";
+        }
+        else if(total>=60 && total<=69){
+
+            document.getElementById("grade3").value= "A-";
+        }
+        else if(total>=50 && total<=59){
+
+            document.getElementById("grade3").value= "B";
+        }
+        else if(total>=40 && total<=49){
+
+            document.getElementById("grade3").value= "C";
+        }
+        else if(total>=33 && total<=39){
+
+            document.getElementById("grade3").value= "D";
+        }
+        else if(total>=0 && total<=32){
+
+            document.getElementById("grade3").value= "F";
+        }
+
+        else {
+
+             alert("It's Not Possible");
+             document.getElementById("tm3").value= "";
+             document.getElementById("pm3").value= "";
+             document.getElementById("ttm3").value= "";
+             document.getElementById("grade3").value= "";
+        }
+
+        if(document.getElementById("grade3").value == "A+" ){
+
+            document.getElementById("point3").value=5.00;
+        }
+       else if(document.getElementById("grade3").value == "A" ){
+
+            document.getElementById("point3").value=4.00;
+        }
+        else if(document.getElementById("grade3").value == "A-" ){
+
+            document.getElementById("point3").value=3.50;
+        }
+        else if(document.getElementById("grade3").value == "B" ){
+
+            document.getElementById("point3").value=3.00;
+        }
+        else if(document.getElementById("grade3").value == "C" ){
+
+            document.getElementById("point3").value=2.00;
+        }
+        else if(document.getElementById("grade3").value == "D" ){
+
+            document.getElementById("point3").value=1.00;
+        }
+
+        else{
+
+            document.getElementById("grade3").value= "F";
+            document.getElementById("point3").value= "0";
+          
+        }
+
+ }
