3d5eb56de6b05be6eeb505f0d0d9adbfec38b6a7
asraf-mridha <asrafmridha@gmail.com>
2022-07-05 21:30:01 +0600

    calculate total point & marks

--- script.js
+++ script.js
@@ -348,6 +348,97 @@ function total3(){
 
          // Another function for General science 
 
+    function total5(){
+
+        var _theorymarks= document.getElementById("tm5").value;
+        var _practicalmarks= document.getElementById("pm5").value;
+    
+         if( _theorymarks=="" || _practicalmarks=="" ){
+    
+       alert("Please Push a number");
+         }
+            var total= parseInt(_practicalmarks)+parseInt(_theorymarks);
+          
+            document.getElementById("ttm5").value=total;
+    
+            if(total>=80 && total<=100){
+    
+                document.getElementById("grade5").value= "A+";
+            }
+            else if(total>=70 && total<=79){
+    
+                document.getElementById("grade5").value= "A";
+            }
+            else if(total>=60 && total<=69){
+    
+                document.getElementById("grade5").value= "A-";
+            }
+            else if(total>=50 && total<=59){
+    
+                document.getElementById("grade5").value= "B";
+            }
+            else if(total>=40 && total<=49){
+    
+                document.getElementById("grade5").value= "C";
+            }
+            else if(total>=33 && total<=39){
+    
+                document.getElementById("grade5").value= "D";
+            }
+            else if(total>=0 && total<=32){
+    
+                document.getElementById("grade5").value= "F";
+            }
+    
+            else {
+    
+                 alert("It's Not Possible");
+                 document.getElementById("tm5").value= "";
+                 document.getElementById("pm5").value= "";
+                 document.getElementById("ttm5").value= "";
+                 document.getElementById("grade5").value= "";
+            }
+    
+            if(document.getElementById("grade5").value == "A+" ){
+    
+                document.getElementById("point5").value=5.00;
+            }
+           else if(document.getElementById("grade5").value == "A" ){
+    
+                document.getElementById("point5").value=4.00;
+            }
+            else if(document.getElementById("grade5").value == "A-" ){
+    
+                document.getElementById("point5").value=3.50;
+            }
+            else if(document.getElementById("grade5").value == "B" ){
+    
+                document.getElementById("point5").value=3.00;
+            }
+            else if(document.getElementById("grade5").value == "C" ){
+    
+                document.getElementById("point5").value=2.00;
+            }
+            else if(document.getElementById("grade5").value == "D" ){
+    
+                document.getElementById("point5").value=1.00;
+            }
+    
+            else{
+    
+                document.getElementById("grade5").value= "0";
+                document.getElementById("point5").value= "0";
+              
+            }
+    
+     }
+
+
+
+
+    //  for islamic studies 
+
+
     function total6(){
 
         var _theorymarks= document.getElementById("tm6").value;
@@ -426,42 +517,79 @@ function total3(){
     
             else{
     
-                document.getElementById("grade6").value= "F";
+                document.getElementById("grade6").value= "0";
                 document.getElementById("point6").value= "0";
               
             }
     
      }
 
-    //  for islamic studies 
+
+
 
 
 
     //   for calculate total result 
  function totalresult(){
 
-   var _bn=document.getElementById("ttm").value;
-   var _en=document.getElementById("ttm1").value;
-   var _math=document.getElementById("ttm3").value;
-   var _soclscience=document.getElementById("ttm4").value;
-   var _gscience=document.getElementById("ttm5").value;
-   var _islamicstd=document.getElementById("ttm6").value;
+     //if fail any subject don't count result
+      var _failpoint=document.getElementById("point").value;
+      var _failpoint1=document.getElementById("point1").value;
+      var _failpoint2=document.getElementById("point3").value;
+      var _failpoint3=document.getElementById("point4").value;
+      var _failpoint4=document.getElementById("point5").value;
+      var _failpoint5=document.getElementById("point6").value;
+      
+    if( _failpoint==0 || _failpoint1==0 || _failpoint3==0 || _failpoint2==0 || _failpoint4== 0 || _failpoint5 ==0  ){
 
-   var _totalmark= parseInt(_bn)+parseInt(_en)+parseInt(_math)+parseInt(_soclscience)+parseInt(_gscience)+parseInt(_islamicstd);
+    document.getElementById("totalMark").value=0;
+    document.getElementById("totalgrade").value="Fail";
+    document.getElementById("totalpoint").value=0;
+      
+    }
 
-   document.getElementById("totalMark").value= _totalmark;
+    else{
 
-   var _bnpoint=document.getElementById("point").value;
-   var _enpoint=document.getElementById("point1").value;
-   var _mathpoint=document.getElementById("point3").value;
-   var _soclscience=document.getElementById("point4").value;
-   var _gsciencepoint=document.getElementById("point5").value;
-   var _islamicstdpoint=document.getElementById("point6").value;
+        var _bn=document.getElementById("ttm").value;
+        var _en=document.getElementById("ttm1").value;
+        var _math=document.getElementById("ttm3").value;
+        var _soclscience=document.getElementById("ttm4").value;
+        var _gscience=document.getElementById("ttm5").value;
+        var _islamicstd=document.getElementById("ttm6").value;
+     
+        var _totalmark= parseInt(_bn)+parseInt(_en)+parseInt(_math)+parseInt(_soclscience)+parseInt(_gscience)+parseInt(_islamicstd);
+     
+        document.getElementById("totalMark").value= _totalmark;
+     
+        var _bnpoint=document.getElementById("point").value;
+        var _enpoint=document.getElementById("point1").value;
+        var _mathpoint=document.getElementById("point3").value;
+        var _soclscience=document.getElementById("point4").value;
+        var _gsciencepoint=document.getElementById("point5").value;
+        var _islamicstdpoint=document.getElementById("point6").value;
+        
+     
+        var _totalpoint= parseInt(_bnpoint)+parseInt(_enpoint)+parseInt(_mathpoint)+parseInt(_soclscience)+parseInt(_gsciencepoint)+parseInt(_islamicstdpoint);
+     
+         document.getElementById("totalpoint").value=_totalpoint/6;
+
+        // if(b=5.00){
+        //    document.getElementById("totalgrade").value="A+";
+        // }
+        // else if(b >=4 ){
+
+        //     document.getElementById("totalgrade").value="A";
+        // }
+     
+         
+     
+
+
+
+    }
+
    
-
-   var _totalpoint= parseInt(_bnpoint)+parseInt(_enpoint)+parseInt(_mathpoint)+parseInt(_soclscience)+parseInt(_gsciencepoint)+parseInt(_islamicstdpoint);
-
-    document.getElementById("totalpoint").value=_totalpoint/6;
+   
    
 
  }
